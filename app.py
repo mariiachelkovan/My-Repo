cc9b45dd818c162d8ba722fbad46d1fb8b59e72d
mariiachelkovan <mariiachelkovan@gmail.com>
2024-10-18 10:31:39 +0300

    Initial commit: basic calculator with add and subtract

---
+++ app.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+if __name__ == "__main__":
+    operation = sys.argv[1]
+    num1 = int(sys.argv[2])
+    num2 = int(sys.argv[3])
+
+    if operation == "add":
+        print(add(num1, num2))
+    elif operation == "subtract":
+        print(subtract(num1, num2))
